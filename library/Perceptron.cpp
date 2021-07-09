82ab25d7907c6cc112006d55c6cfc583dfda724c
altruios <49083558+altruios@users.noreply.github.com>
2021-07-08 19:13:33 -0700

    updates, still considering design

--- library/Perceptron.cpp
+++ library/Perceptron.cpp
@@ -12,10 +12,14 @@ Perceptron::Perceptron(float x,float y, float offset){
      sf::FloatRect shape_bounds = this->shape.getLocalBounds();
      this->shape.setOrigin(shape_bounds.width/2.f, shape_bounds.height/2.f);
 }
+
 void Perceptron::update(){
      this->shape.setPosition(sf::Vector2f{this->x*this->offset+offset,this->y*this->offset+offset});
-     this->shape.setRadius((20));
-
+     this->shape.setRadius(this->Activation*40);
+     this->shape.setOutlineThickness(this->bias*5);
+     this->shape.setOutlineColor(this->Activation>0?sf::Color::Red:sf::Color::Blue);
+     sf::FloatRect shape_bounds = this->shape.getLocalBounds();
+     this->shape.setOrigin(shape_bounds.width/2.f, shape_bounds.height/2.f);
 
 }
 void Perceptron::render(sf::RenderWindow &window){
@@ -32,8 +36,11 @@ void Perceptron::activate(std::vector<float>&weights, std::vector<float>&activat
      act+=bias;
      this->Activation=this->activation_fn(act);
 }
-void Perceptron::set_weight(float weight){
-     this->weight=weight;
+float Perceptron::get_weight(int key){
+     return this->weights[key];
+}
+void Perceptron::set_weights(float weight,int index){
+     this->weights[index]=weight;
 }
 void Perceptron::set_bias(float bias){
      this->bias=bias;
@@ -44,4 +51,3 @@ float Perceptron::activation_fn(float value){
 void Perceptron::debug(sf::RenderWindow &window){
      window.draw(this->debug_box);     
 }
-
